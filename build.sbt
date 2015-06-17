f6dac954fa7b2cb2d9be3c69c060fd3c59f3ba58
Alexey Alekhin <laughedelic@gmail.com>
2015-06-17 14:12:08 +0200

    Wrote test (took from statika-cli); tried on velvet

--- build.sbt
+++ build.sbt
@@ -9,7 +9,7 @@ publishBucketSuffix := "era7.com"
 resolvers := Seq(
   "Era7 public maven releases"  at s3("releases.era7.com").toHttps(s3region.value.toString),
   "Era7 public maven snapshots" at s3("snapshots.era7.com").toHttps(s3region.value.toString)
-)
+) ++ resolvers.value
 
 libraryDependencies ++= Seq(
   "ohnosequences" %% "statika" % "2.0.0-SNAPSHOT",
--- src/test/scala/ohnosequences/statika-distributions/Tests.scala
+++ src/test/scala/ohnosequences/statika-distributions/Tests.scala
@@ -8,6 +8,7 @@ import ohnosequences.awstools.ec2._
 import ohnosequences.awstools.ec2.{Tag => Ec2Tag}
 import java.io._
 import org.scalatest._
+import scala.annotation.tailrec
 import com.amazonaws.auth._, profile._
 import ohnosequences.awstools.regions.Region._
 import ohnosequences.awstools.ec2.InstanceType._
@@ -15,6 +16,36 @@ import ohnosequences.awstools.ec2.InstanceType._
 import era7.project._, era7.aws._, defs._
 
 class ApplicationTest extends FunSuite with ParallelTestExecution {
+
+  def launchAndWait(ec2: EC2, name: String, specs: InstanceSpecs, number: Int = 1): List[ec2.Instance] = {
+    // TODO: run instances in parallel
+    ec2.runInstances(number, specs) flatMap { inst =>
+      def checkStatus: String = inst.getTagValue("statika-status").getOrElse("...")
+
+      val id = inst.getInstanceId()
+      def printStatus(s: String) = println(name+" ("+id+"): "+s)
+
+      inst.createTag(Ec2Tag("Name", name))
+      printStatus("launched")
+
+      while(checkStatus != "preparing") { Thread sleep 2000 }
+      printStatus("url: "+inst.getPublicDNS().getOrElse("..."))
+
+      @tailrec def waitForSuccess(previous: String): String = {
+        val current = checkStatus
+        if(current == "failure" || current == "success") {
+          printStatus(current)
+          current
+        } else {
+          if (current != previous) printStatus(current)
+          Thread sleep 3000
+          waitForSuccess(current)
+        }
+      }
+
+      if (waitForSuccess(checkStatus) == "success") Some(inst) else None
+    }
+  }
 
 
   case object bundlesTest extends Project("BundlesTest")
@@ -30,14 +61,11 @@ class ApplicationTest extends FunSuite with ParallelTestExecution {
   val ec2 = EC2.create(new ProfileCredentialsProvider("default"))
 
   test("trying to launch an instance") {
-    println(conf.specs.userData)
-    ec2.runInstances(1, conf.specs)
+    // println(conf.specs.userData)
+    val N = 1
+    val instances = launchAndWait(ec2, conf.comp.name, conf.specs, N)
+    instances.foreach{ _.terminate }
+    assert{ instances.length == N }
   }
 
 }
-
-// val result = ec2.applyAndWait(bundle.name, specs, 1) match {
-//   case List(inst) => inst.getTagValue("statika-status") == Some("success")
-//   case _ => false
-// }
-// assert(result)
